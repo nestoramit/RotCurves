39270660f1f1006b951d10852a1279154d9d4369
nestoramit <45100100+nestoramit@users.noreply.github.com>
2025-07-11 23:37:04 +0300

    updates

--- RotCurves/rotation_curve.py
+++ RotCurves/rotation_curve.py
@@ -132,8 +132,6 @@ class RotationCurveObject:
 
                 self.sigma_beam_x = self.sigma_beam
                 self.sigma_beam_y = self.sigma_beam * geometrical_factor_elliptical
-                # TODO: THIS IS AN ERROR, ROUNDING SIGMA_BEAM ALTERS THE SIZE OF THE BEAM
-                # TODO: IT SHOULD BE ROUNDED DOWN AND HANDLE THE LEFTOVERS CAREFULLY
                 self.sigma_beam_pixels_x = int(np.ceil(self.sigma_beam_x / self.dx))
                 self.sigma_beam_pixels_y = int(np.ceil(self.sigma_beam_y / self.dx))
                 self.oversample_edge_pixels_x = int(np.ceil(self.oversample_edge * self.sigma_beam_pixels_x))
